9b49cf1b0434a7ab8510b5c7ff625ab0917a1e03
GitCoder052023 <K.alam93899@gmail.com>
2025-01-26 20:17:13 +0530

    refactor: clean up LandingPage and 404 templates; improve navigation and styling

--- Public/scripts/LandingPage.js
+++ Public/scripts/LandingPage.js
@@ -1,11 +1,5 @@
-const GetStarted_Btn = document.getElementById("GetStarted_Btn")
-const mobileMenuButton = document.getElementById('mobile-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
+const GetStarted_Btn = document.getElementById("GetStarted_Btn");
 
-mobileMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-});
-
-GetStarted_Btn.addEventListener("click", function() {
-  window.location.href = "/chat";
+GetStarted_Btn.addEventListener("click", function () {
+    window.location.href = "/chat";
 });
