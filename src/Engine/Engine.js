2f84fb414153c45c86ae2a2938c755b111cb7889
GitCoder052023 <K.alam93899@gmail.com>
2025-01-30 11:29:23 +0530

    feat: migrate server implementation to Engine.js and update start script in package.json

--- src/Engine/Engine.js
+++ src/Engine/Engine.js
@@ -7,14 +7,11 @@ const sslOptions = require('./config/ssl.config');
 const { PORT } = require('./config/app.config');
 const setupSocketIO = require('./sockets/connectionHandler');
 
-// Create HTTPS server
 const server = https.createServer(sslOptions, app);
 const io = socketIO(server);
 
-// Initialize Socket.IO handlers
 setupSocketIO(io);
 
-// Start server
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
